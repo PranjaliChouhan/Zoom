ea3891a679414c113394924a3ade0e75bb3f53b4
PranjaliChouhan <pranjalichouhan4@gmail.com>
2023-08-03 20:37:02 +0530

    initial

--- zoom/src/Zoom.js
+++ zoom/src/Zoom.js
@@ -1,26 +1,29 @@
 import React, { useReducer } from 'react';
 import { ImZoomIn, ImZoomOut } from 'react-icons/im';
 
-
+const MIN_ZOOM = 0.5;
+  const MAX_ZOOM = 3;
 // Reducer function to handle image zoom state
 const imageZoomReducer = (state, action) => {
-  switch (action.type) {
-    case 'ZOOM_IN':
-      return { ...state, zoomLevel: state.zoomLevel + 0.1 };
-    case 'ZOOM_OUT':
-      return { ...state, zoomLevel: state.zoomLevel - 0.1 };
-    case 'RESET_ZOOM':
-      return { ...state, zoomLevel: 1, positionX: 0, positionY: 0 };
-    case 'PAN_IMAGE':
-      return { ...state, positionX: action.positionX, positionY: action.positionY };
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+      case 'ZOOM_IN':
+        return { ...state, zoomLevel: Math.min(state.zoomLevel + 0.1, MAX_ZOOM) };
+      case 'ZOOM_OUT':
+        return { ...state, zoomLevel: Math.max(state.zoomLevel - 0.1, MIN_ZOOM) };
+      case 'RESET_ZOOM':
+        return { ...state, zoomLevel: 1, positionX: 0, positionY: 0 };
+      case 'PAN_IMAGE':
+        return { ...state, positionX: action.positionX, positionY: action.positionY };
+      default:
+        return state;
+    }
+  };
 
 const ZoomableImage = ({ imageUrl }) => {
   const initialState = { zoomLevel: 1, positionX: 0, positionY: 0 };
   const [state, dispatch] = useReducer(imageZoomReducer, initialState);
+  
+ 
 
   const handleZoomIn = () => {
     dispatch({ type: 'ZOOM_IN' });
@@ -34,23 +37,24 @@ const ZoomableImage = ({ imageUrl }) => {
     dispatch({ type: 'RESET_ZOOM' });
   };
 
-  const preventDefaultTouch = (event) => {
-    event.preventDefault();
-  };
+  
 
   return (
     <>
-    <div className="image-container" onTouchStart={() => {}}>
-      <div style={{ transform: `scale(${state.zoomLevel}) translate(${state.positionX}px, ${state.positionY}px)` }}>
+    <div className='zoom'>
+     <div style={{ 
+          transform: `scale(${Math.min(Math.max(state.zoomLevel, MIN_ZOOM), MAX_ZOOM)}) translate(${state.positionX}px, ${state.positionY}px)`
+           // Set transform origin to top left when shouldFlip is true
+       }}>
+        <div className='img'>
         <img
           className='image-container'
           src={imageUrl}
           alt="ZoomableImage"
-          onTouchStart={preventDefaultTouch}
-          onTouchMove={preventDefaultTouch}
-          onTouchEnd={preventDefaultTouch}
+         
         
         />
+        </div>
       </div>
       <div className='btn'>
       <button className='btn-handle' onClick={handleZoomIn}>Zoom-In <ImZoomIn style={{ marginLeft: '6px' }}/></button>
